9b0765cf0f535eaf6148a913b1187cb2788a8254
Fatih Akturk <f.akturk@gmail.com>
2017-02-13 22:20:41 +0900

    asd

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,9 +6,13 @@
 #include <unistd.h>
 #include <android/log.h>
 #include <time.h>
+#include <iostream>
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "TestJNIActivity", __VA_ARGS__))
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "DebugJNIActivity", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "ErrorJNIActivity", __VA_ARGS__))
 #define LOOPER_ID 1
 #define SAMP_PER_SEC 100
+
 
 
 
@@ -22,6 +26,8 @@ int64_t lastGyroTime = 0;
 int magCounter = 0;
 int64_t lastMagTime = 0;
 
+float cAcc[3];
+
 static int get_sensor_events(int fd, int events, void* data);
 struct tm* start;
 struct tm* finish;
@@ -30,9 +36,58 @@ extern "C"
 jstring
 Java_netlab_fakturk_nativeacc_MainActivity_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */) {
+        jobject thiz) {
     std::string hello = "Hello from C++";
+//    LOGD("hebele");
+
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_netlab_fakturk_nativeacc_MainActivity_setAccXFromC(
+        JNIEnv *env,
+        jobject thiz, jfloat a)
+{
+    jclass clazz = (*env).GetObjectClass(thiz);
+    jmethodID setAccX = env->GetMethodID(clazz, "setAccX", "(F)V");
+
+    env->CallVoidMethod(thiz,setAccX,4.0);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_netlab_fakturk_nativeacc_MainActivity_setAccYFromC(
+        JNIEnv *env,
+        jobject thiz, jfloat a)
+{
+    jclass clazz = (*env).GetObjectClass(thiz);
+    jmethodID setAccY = env->GetMethodID(clazz, "setAccY", "(F)V");
+
+    env->CallVoidMethod(thiz,setAccY,5.0);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_netlab_fakturk_nativeacc_MainActivity_setAccZFromC(
+        JNIEnv *env,
+        jobject thiz, jfloat a)
+{
+    jclass clazz = (*env).GetObjectClass(thiz);
+    jmethodID setAccZ = env->GetMethodID(clazz, "setAccZ", "(F)V");
+
+    env->CallVoidMethod(thiz,setAccZ,6.0);
+}
+
+extern "C"
+jfloatArray Java_netlab_fakturk_nativeacc_MainActivity_getAcc(JNIEnv *env, jobject thiz)
+{
+    jfloatArray acc = (*env).NewFloatArray(3);
+    float cAcc[3];
+    cAcc[0]=0;
+    cAcc[1]=1;
+    cAcc[2]=2;
+    (*env).SetFloatArrayRegion(acc,0,3,cAcc);
+    return acc;
 }
 
 extern "C"
@@ -42,9 +97,11 @@ void Java_netlab_fakturk_nativeacc_MainActivity_sensorValue(JNIEnv* env, jobject
     int events, ident;
     ASensorManager* sensorManager;
     const ASensor* accSensor;
-    const ASensor* gyroSensor;
-    const ASensor* magSensor;
+//    const ASensor* gyroSensor;
+//    const ASensor* magSensor;
     void* sensor_data = malloc(1000);
+
+
 
     LOGI("sensorValue() - ALooper_forThread()");
 
@@ -58,28 +115,41 @@ void Java_netlab_fakturk_nativeacc_MainActivity_sensorValue(JNIEnv* env, jobject
     sensorManager = ASensorManager_getInstance();
 
     accSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_ACCELEROMETER);
-    gyroSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_GYROSCOPE);
-    magSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_MAGNETIC_FIELD);
+//    gyroSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_GYROSCOPE);
+//    magSensor = ASensorManager_getDefaultSensor(sensorManager, ASENSOR_TYPE_MAGNETIC_FIELD);
 
 
 
     sensorEventQueue = ASensorManager_createEventQueue(sensorManager, looper, 3, get_sensor_events, sensor_data);
 
     ASensorEventQueue_enableSensor(sensorEventQueue, accSensor);
-    ASensorEventQueue_enableSensor(sensorEventQueue, gyroSensor);
-    ASensorEventQueue_enableSensor(sensorEventQueue, magSensor);
+//    ASensorEventQueue_enableSensor(sensorEventQueue, gyroSensor);
+//    ASensorEventQueue_enableSensor(sensorEventQueue, magSensor);
 
     //Sampling rate: 100Hz
     int a = ASensor_getMinDelay(accSensor);
-    int b = ASensor_getMinDelay(gyroSensor);
-    int c = ASensor_getMinDelay(magSensor);
-    LOGI("min-delay: %d, %d, %d",a,b,c);
+//    int b = ASensor_getMinDelay(gyroSensor);
+//    int c = ASensor_getMinDelay(magSensor);
+//    LOGI("min-delay: %d, %d, %d",a,b,c);
+
+    LOGI("min-delay: %d",a);
     ASensorEventQueue_setEventRate(sensorEventQueue, accSensor, 100000);
-    ASensorEventQueue_setEventRate(sensorEventQueue, gyroSensor, 100000);
-    ASensorEventQueue_setEventRate(sensorEventQueue, magSensor, 100000);
+
+//    ASensorEventQueue_setEventRate(sensorEventQueue, gyroSensor, 100000);
+//    ASensorEventQueue_setEventRate(sensorEventQueue, magSensor, 100000);
+//    std::cout<<"fatih"<<std::endl;
+//    LOGD("akturk");
 
     LOGI("sensorValue() - START");
+
+        jclass clazz = (*env).GetObjectClass(thiz);
+    jmethodID setAccX = env->GetMethodID(clazz, "setAccX", "(F)V");
+
+    env->CallVoidMethod(thiz,setAccX,cAcc[0]);
+
 }
+
+
 
 
 static int get_sensor_events(int fd, int events, void* data) {
@@ -87,15 +157,20 @@ static int get_sensor_events(int fd, int events, void* data) {
     //ASensorEventQueue* sensorEventQueue;
     while (ASensorEventQueue_getEvents(sensorEventQueue, &event, 1) > 0) {
         if(event.type == ASENSOR_TYPE_ACCELEROMETER) {
-            //LOGI("accl(x,y,z,t): %f %f %f %lld", event.acceleration.x, event.acceleration.y, event.acceleration.z, event.timestamp);
-            if(accCounter == 0 || accCounter == 1000)
-            {
-                LOGI("Acc-Time: %lld (%f)", event.timestamp,((double)(event.timestamp-lastAccTime))/1000000000.0);
-                lastAccTime = event.timestamp;
-                accCounter = 0;
-            }
+            cAcc[0] = event.acceleration.x;
+            cAcc[1] = event.acceleration.y;
+            cAcc[2] = event.acceleration.z;
 
-            accCounter++;
+
+//            LOGI("accl(x,y,z,t): %f %f %f %lld", event.acceleration.x, event.acceleration.y, event.acceleration.z, event.timestamp);
+//            if(accCounter == 0 || accCounter == 1000)
+//            {
+//                LOGI("Acc-Time: %lld (%f)", event.timestamp,((double)(event.timestamp-lastAccTime))/1000000000.0);
+                lastAccTime = event.timestamp;
+//                accCounter = 0;
+//            }
+
+//            accCounter++;
         }
         else if(event.type == ASENSOR_TYPE_GYROSCOPE) {
             //LOGI("accl(x,y,z,t): %f %f %f %lld", event.acceleration.x, event.acceleration.y, event.acceleration.z, event.timestamp);
--- app/src/main/java/netlab/fakturk/nativeacc/MainActivity.java
+++ app/src/main/java/netlab/fakturk/nativeacc/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    float[] acc;
 
     // Used to load the 'native-lib' library on application startup.
     static
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        acc = new float[3];
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -24,12 +26,42 @@ public class MainActivity extends AppCompatActivity
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
         sensorValue();
+        acc = getAcc();
+        System.out.println(acc[0]+", "+acc[1]+", "+acc[2]);
+//        setAccXFromC(acc[0]);
+//        setAccYFromC(acc[0]);
+//        setAccZFromC(acc[0]);
     }
+
+     void setAccX(float a)
+    {
+
+
+        System.out.print(a);
+    }
+    void setAccY(float a)
+    {
+
+
+        System.out.print(" "+a);
+    }
+    void setAccZ(float a)
+    {
+
+
+        System.out.println(" "+a);
+    }
+
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
-    public static native void sensorValue();
+    public  native void sensorValue();
+    public native float[] getAcc();
+    public native void setAccXFromC(float a);
+    public native void setAccYFromC(float a);
+    public native void setAccZFromC(float a);
 }
